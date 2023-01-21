72bfe60cb1214a216060a9e533851ac06a41b879
MatthewNorton <mattnorton.codes@gmail.com>
2023-01-21 14:09:27 -0500

    fixing git cache

--- src/utils/Shuffle.tsx
+++
@@ -1,9 +0,0 @@
-// Utility
-export const Shuffle = (array: any[]) => {
-    let arrayClone = array.slice(0); // cloning array to prevent shuffle on original load.
-    for (let i = arrayClone.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arrayClone[i], arrayClone[j]] = [arrayClone[j], arrayClone[i]];
-    }
-    return arrayClone;
-  };
